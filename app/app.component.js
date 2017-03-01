52dc5b3906dd7bb3c29fcec33d3d241cd8d3ea24
Bahareh SadeghianBoroujeni <sadeghib@student.tut.fi>
2017-03-01 22:18:10 +0200

    angular files created

---
+++ app/app.component.js
@@ -0,0 +1,124 @@
+System.register(["@angular/core"], function (exports_1, context_1) {
+    "use strict";
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __moduleName = context_1 && context_1.id;
+    var core_1, Hero, HEROES, AppComponent;
+    return {
+        setters: [
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }
+        ],
+        execute: function () {
+            Hero = class Hero {
+            };
+            exports_1("Hero", Hero);
+            HEROES = [
+                { id: 11, name: 'Mr. Nice' },
+                { id: 12, name: 'Narco' },
+                { id: 13, name: 'Bombasto' },
+                { id: 14, name: 'Celeritas' },
+                { id: 15, name: 'Magneta' },
+                { id: 16, name: 'RubberMan' },
+                { id: 17, name: 'Dynama' },
+                { id: 18, name: 'Dr IQ' },
+                { id: 19, name: 'Magma' },
+                { id: 20, name: 'Tornado' }
+            ];
+            AppComponent = class AppComponent {
+                constructor() {
+                    this.title = 'Tour of Heroes';
+                    this.heroes = HEROES;
+                }
+                onSelect(hero) {
+                    this.selectedHero = hero;
+                }
+            };
+            AppComponent = __decorate([
+                core_1.Component({
+                    selector: 'my-app',
+                    template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
+    </div>
+  `,
+                    styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
+                })
+            ], AppComponent);
+            exports_1("AppComponent", AppComponent);
+            /*
+            Copyright 2017 Google Inc. All Rights Reserved.
+            Use of this source code is governed by an MIT-style license that
+            can be found in the LICENSE file at http://angular.io/license
+            */
+        }
+    };
+});
+//# sourceMappingURL=app.component.js.map
