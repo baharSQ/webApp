9c697353f78533e5896fc6d8b9ebf355d73d9bb4
Bahareh SadeghianBoroujeni <sadeghib@student.tut.fi>
2017-03-03 13:46:36 +0200

    zoomin and out anime addet to pictures

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function zoomIn(x) {
+//function zoomIn(x) {
 
-          $(x).css("background-size", "110%");
-}
-function zoomOut(x) {
+        //  $(x).css("background-size", "140%");
 
-          $(x).css("background-size", "100%");
-}
+//        $(x).animate({background-size: '140%'},3000);
+//}
+
+$(document).ready(function(){
+  $('#a').mouseenter(function(){$(this).animate({backgroundSize: '120%'},1000)});
+  $('#a').mouseout(function(){$(this).stop().css({backgroundSize: '100%'})});
+
+})
+
+//function zoomOut(x) {
+
+//          $(x).css("background-size", "100%");
+//}
+//$(document).ready(function(){
+//  $('#a').mouseout(function(){$(this).animate({backgroundSize: '100%'})});
+//})
